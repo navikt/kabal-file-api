f1588cb5c5be8508a91c83710ee0baab6d15eafd
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-12-02 12:45:25 +0100

    Upgrade Spring framework to last 3 version. Remove e2e reference.

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     val kotlinVersion = "2.2.0"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
-    id("org.springframework.boot") version "3.5.3"
+    id("org.springframework.boot") version "3.5.8"
     id("io.spring.dependency-management") version "1.1.7"
     idea
 }
