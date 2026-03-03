6cf71517507b2ad9585c6bdfbe34c9018d62de7e
Andreas Jonsson <andreas.jonsson@nav.no>
2026-03-03 10:45:58 +0100

    Override dependencies until Spring has updated.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,10 @@ plugins {
     idea
 }
 
+// CVE GHSA-72hv-8253-57qq: jackson-core async parser DoS. Remove when Spring has updated.
+extra["jackson-2-bom.version"] = "2.21.1"
+extra["jackson-bom.version"] = "3.1.0"
+
 dependencies {
     implementation(kotlin("stdlib"))
 
